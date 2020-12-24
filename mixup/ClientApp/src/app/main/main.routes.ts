e607f0c9157274eec69b9963fdf51cee08bf69e2
Matt Stratton <mattstrattn@gmail.com>
2020-12-23 20:02:01 -0500

    add buttons to layout and update styles

--- mixup/ClientApp/src/app/main/main.routes.ts
+++ mixup/ClientApp/src/app/main/main.routes.ts
@@ -2,16 +2,21 @@ import { Route } from '@angular/router';
 import { FetchDataComponent, HomeComponent } from './components';
 
 export const routes: Route[] = [
-    {
-        path: '',
-        component: HomeComponent
-    },
-    {
-        path: 'fetch-data',
-        component: FetchDataComponent
-    },
-    // {
-    //     path: 'sample',
-    //     loadChildren: '../sample/sample.module#SampleModule'
-    // },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'fetch-data',
+    component: FetchDataComponent
+  },
+  // {
+  //     path: 'sample',
+  //     loadChildren: '../sample/sample.module#SampleModule'
+  // },
 ];
